b2f3c6da9ce349b6a7eac0424515e55245795b7c
arif-05 <arifcse949@gmail.com>
2024-02-26 12:15:39 +0600

    complited

--- A_Division.cpp
+++ A_Division.cpp
@@ -4,6 +4,15 @@
 using namespace std;
 
 int main(){
-    
+    int t, n;
+    cin>>t;
+    while(t--){
+        n=0;
+        cin>>n;
+        if(n>=1900) cout<<"Division 1"<<endl;
+        else if(n>=1600) cout<<"Division 2"<<endl;
+        else if(n>=1400) cout<<"Division 3"<<endl;
+        else cout<<"Division 4"<<endl;
+    }
     return 0;
 }
