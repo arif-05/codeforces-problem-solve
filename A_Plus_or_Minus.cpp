548e0f13df912e8922b60a9bf88c36767ef011c3
arif-05 <arifcse949@gmail.com>
2024-05-07 14:12:32 +0600

    new problem solved

---
+++ A_Plus_or_Minus.cpp
@@ -0,0 +1,17 @@
+//Bismillahir Rahmanir Rahim
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,t;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c;
+        if(a+b==c)cout<<"+"<<endl;
+        else cout<<"-"<<endl;
+    }
+
+
+    return 0;
+}
